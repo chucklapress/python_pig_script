c460214f8daa51632927e5a5e98b138082f7b113
chucklapress <charleslapress@gmail.com>
2020-06-03 22:21:40 +0000

    modified comment to reflect new pig latin script being called

--- oink.py
+++ oink.py
@@ -7,7 +7,7 @@ lines = LOAD '/home/chuck/test.txt' AS (line:chararray);
 words = FOREACH lines GENERATE FLATTEN(TOKENIZE(line)) as word;
 grouped = GROUP words BY word;
 wordcount = FOREACH grouped GENERATE group, COUNT(words);
-DUMP wordcount;
+STORE wordcount INTO '/home/chuck/python_piggy/result.txt' using PigStorage(';');
 """
 import subprocess as s
 pigpath = [ "pig","/home/chuck/output.pig"]

7ed46b2bf0e4fe8fb734cc57a8a641315e0df167
chucklapress <charleslapress@gmail.com>
2020-06-03 22:02:00 +0000

    initial commit with py pig script

---
+++ oink.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+#pig script it's calling:
+
+"""
+lines = LOAD '/home/chuck/test.txt' AS (line:chararray);
+words = FOREACH lines GENERATE FLATTEN(TOKENIZE(line)) as word;
+grouped = GROUP words BY word;
+wordcount = FOREACH grouped GENERATE group, COUNT(words);
+DUMP wordcount;
+"""
+import subprocess as s
+pigpath = [ "pig","/home/chuck/output.pig"]
+oink = s.run(pigpath)
+
+print("Th-Th-The, Th-Th-The, Th-Th... That's all, folks!")
+
